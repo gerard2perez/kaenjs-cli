baa767dee35f1a4f52732fd9113828b9936050c3
Gerardo Pérez <gerard2perez@outlook.com>
2019-04-22 17:43:08 -0500

    fix: build resources in production mode by default

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,23 +1,26 @@
-import { Parsed, text } from '@gerard2p/mce';
+import { Parsed, text, enumeration } from '@gerard2p/mce';
 import { error, ok } from '@gerard2p/mce/console';
 import { cp, printRelativePath, targetPath, spinSpawn } from '@gerard2p/mce/utils';
 import * as fb from 'fast-glob';
-import { existsSync, mkdirSync, readFile, writeFile } from 'fs';
+import { existsSync, mkdirSync, readFile, writeFile, unlinkSync, statSync, readdirSync } from 'fs';
 import { dirname, posix } from 'path';
 import { minify } from 'uglify-es';
 import { promisify } from 'util';
 export let description = 'Build your project for production';
 // export let args = '<arg1> [varidac...]';
 export let options = {
+	env: enumeration('-e <env>', 'environment', ['development', 'production'], 'production'),
 	config: text('-c <config>', 'Configuratión source file', 'tsconfig.dist.json'),
 	output: text('-o <output>', 'Configuratión source file', 'dist'),
 
 };
 export async function action(opt:Parsed<typeof options>) {
 	await spinSpawn('Compiling', 'npx', ['tsc', '-b', opt.config]);
-	await spinSpawn('Resources', 'kn', ['resources']);
+	await spinSpawn('Resources', 'kn', ['resources', '-e', opt.env]);
 	let { compilerOptions:{outDir} } = require(targetPath(opt.config));
-	let res = await fb<string>(`${outDir}/**/*.js`);
+	for(const asset of await fb<string>(posix.join(opt.output, 'public', '**', '*.*'))) {
+		unlinkSync(asset);
+	}
 	for(const asset of await fb<string>(posix.join('public', '**', '*.*'))) {
 		let target = posix.join(opt.output,asset);
 		ensureDir(dirname(target));
@@ -28,6 +31,15 @@ export async function action(opt:Parsed<typeof options>) {
 		ensureDir(dirname(target));
 		cp(asset, posix.join(opt.output,asset));
 	}
+	for(const script of (await fb<string>(`${outDir}/**/*.js`)).reverse()) {
+		let target = posix.join(opt.output,script.replace(outDir, ''));
+		ensureDir(dirname(target));
+		await compress(script, target);
+		// unlinkSync(script);
+		// if(readdirSync(dirname(script)).length === 0)
+		// 	unlinkSync(dirname(script));
+	}
+	// unlinkSync(outDir);
 	cp('package.dist.json', posix.join(opt.output, 'package.json'));
 }
 async function compress(source:string, output:string) {
